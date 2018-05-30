52a00853a0cfe2e217ec9d435f1b89256f6f4ce6
G0tti <38687072+G0tti@users.noreply.github.com>
2018-05-30 20:50:38 +0200

    feat: benotet/unbenotet per bool

--- gemeinsamforschen/src/main/java/unipotsdam/gf/modules/assessment/controller/Assessment.java
+++ gemeinsamforschen/src/main/java/unipotsdam/gf/modules/assessment/controller/Assessment.java
@@ -2,11 +2,31 @@ package unipotsdam.gf.modules.assessment.controller;
 
 public class Assessment {
     private StudentIdentifier student;
+    private GroupIdentifier group;
     private Performance performance;
+    private boolean rated;
 
-    public Assessment(StudentIdentifier student, Performance performance) {
+    public Assessment(StudentIdentifier student, Performance performance, GroupIdentifier group, boolean rated) {
         this.student = student;
         this.performance = performance;
+        this.group=group;
+        this.rated=rated;
+    }
+
+    public boolean isRated() {
+        return rated;
+    }
+
+    public void setRated(boolean rated){
+        this.rated=rated;
+    }
+
+    public GroupIdentifier getGroup() {
+        return group;
+    }
+
+    public void setGroup(GroupIdentifier group) {
+        this.group = group;
     }
 
     public StudentIdentifier getStudent() {
